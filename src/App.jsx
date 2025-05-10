18228bb583fbc558eaff443a245ce5372495f8f8
EdgarMrt26 <edgarmartinez09m@gmail.com>
2025-05-09 22:14:38 -0600

    Estructura Front-end + Archivos.

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./views/Login";
+import Inicio from "./views/Inicio";
+import './App.css';
+import Encabezado from "./components/encabezado/Encabezado";
+import Clientes from "./views/Clientes";
+import Productos from "./views/Productos";
+import Usuarios from "./views/Usuarios";
+import Ventas from "./views/Ventas";
+import Categorias from "./views/Categorias";
+import Proveedores from "./views/Proveedores";
+import Marcas from "./views/Marcas";
+import Compras from "./views/Compras";
+
+
+const App = () => {
+  return (
+    <Router>
+      <main className="margen-superior-main">
+      <Encabezado />
+       <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/inicio" element={<Inicio />} />
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/usuarios" element={<Usuarios />} />
+          <Route path="/ventas" element={<Ventas />} />
+          <Route path="/categorias" element={<Categorias />} />
+          <Route path="/proveedores" element={<Proveedores />} />
+          <Route path="/marcas" element={<Marcas />} />
+          <Route path="/compras" element={<Compras />} />
+      </Routes>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
