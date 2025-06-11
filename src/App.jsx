42243e7e77273074447f9fc9a0786da1c9804586
wesling18 <bermudezwesling@gmail.com>
2025-06-11 14:35:45 -0600

    Corrección de APP.JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./views/Login";
 import Inicio from "./views/Inicio";
-import './App.css';
+import "./App.css";
 import Encabezado from "./components/encabezado/Encabezado";
 import Clientes from "./views/Clientes";
 import Productos from "./views/Productos";
@@ -15,29 +15,37 @@ import Compras from "./views/Compras";
 import Estadisticas from "./views/Estadisticas";
 import Catalogo from "./views/CatalogoProductos";
 import Dashboard from "./views/Dashboard";
-
+import Dashboard2 from "./views/Dashboard2";
+import RutaProtegida from "./components/Rutas/RutaProtegida";
+import Dashboard1 from "./views/Dashboard1";
+import PiePagina from "./components/infopie/PiePagina";
 
 const App = () => {
   return (
     <Router>
-      <main className="margen-superior-main">
-      <Encabezado />
-      <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/inicio" element={<Inicio />} />
-          <Route path="/clientes" element={<Clientes />} />
-          <Route path="/productos" element={<Productos />} />
-          <Route path="/usuarios" element={<Usuarios />} />
-          <Route path="/ventas" element={<Ventas />} />
-          <Route path="/categorias" element={<Categorias />} />
-          <Route path="/proveedores" element={<Proveedores />} />
-          <Route path="/marcas" element={<Marcas />} />
-          <Route path="/compras" element={<Compras />} />
-          <Route path="/estadisticas" element={<Estadisticas />} />
-          <Route path="/catalogo" element={<Catalogo />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-      </main>
+      <div className="app-wrapper">
+        <Encabezado />
+        <main className="margen-superior-main content">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/inicio" element={<RutaProtegida vista={<Inicio />} />} />
+            <Route path="/clientes" element={<RutaProtegida vista={<Clientes />} />} />
+            <Route path="/productos" element={<RutaProtegida vista={<Productos />} />} />
+            <Route path="/categorias" element={<RutaProtegida vista={<Categorias />} />} />
+            <Route path="/ventas" element={<RutaProtegida vista={<Ventas />} />} />
+            <Route path="/usuarios" element={<RutaProtegida vista={<Usuarios />} />} />
+            <Route path="/proveedores" element={<RutaProtegida vista={<Proveedores />} />} />
+            <Route path="/marcas" element={<RutaProtegida vista={<Marcas />} />} />
+            <Route path="/compras" element={<RutaProtegida vista={<Compras />} />} />
+            <Route path="/estadisticas" element={<RutaProtegida vista={<Estadisticas />} />} />
+            <Route path="/catalogo" element={<RutaProtegida vista={<Catalogo />} />} />
+            <Route path="/dashboard" element={<RutaProtegida vista={<Dashboard />} />} />
+            <Route path="/dashboard2" element={<RutaProtegida vista={<Dashboard2 />} />} />
+            <Route path="/dashboard1" element={<RutaProtegida vista={<Dashboard1 />} />} />
+          </Routes>
+        </main>
+        <PiePagina />
+      </div>
     </Router>
   );
 };
